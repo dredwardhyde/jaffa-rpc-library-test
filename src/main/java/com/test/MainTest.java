7cac35ea5f43a80670f650f82cc507693dafeeeb
komesergey <komesergey@gmail.com>
2020-05-16 01:20:20 +0300

    Updated code to current version

--- src/main/java/com/test/MainTest.java
+++ src/main/java/com/test/MainTest.java
@@ -8,19 +8,20 @@ import com.test.services.ServiceCallback;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class MainTest {
 
     private static final int CONCURRENCY_LEVEL = 6;
+    private static final String TARGET_MODULE_ID = "main.server";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("jaffa-rpc-config", "./jaffa-rpc-config-test-server.properties");
-
-        List<Thread> threadList = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(CONCURRENCY_LEVEL);
 
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(MainConfig.class);
@@ -30,47 +31,31 @@ public class MainTest {
         PersonServiceClient personService = ctx.getBean(PersonServiceClient.class);
 
         Runnable runnable = () -> {
-            Integer id = personService.add("James Carr", "james@zapier.com", null).withTimeout(15_000).onModule("main.server").executeSync();
+            Integer id = personService.add("James Carr", "james@zapier.com", null).withTimeout(15_000).onModule(TARGET_MODULE_ID).executeSync();
             log.info("Resulting id is {}", id);
-            Person person = personService.get(id).onModule("main.server").executeSync();
+            Person person = personService.get(id).onModule(TARGET_MODULE_ID).executeSync();
             log.info(String.valueOf(person));
             personService.lol().executeSync();
             personService.lol2("kek").executeSync();
             String personName = personService.getName().executeSync();
             log.info("Name: {}", personName);
-            clientService.lol3("test3").onModule("main.server").executeSync();
-            clientService.lol4("test4").onModule("main.server").executeSync();
-            clientService.lol4("test4").onModule("main.server").executeAsync(UUID.randomUUID().toString(), ServiceCallback.class);
-            personService.get(id).onModule("main.server").executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
+            clientService.lol3("test3").onModule(TARGET_MODULE_ID).executeSync();
+            clientService.lol4("test4").onModule(TARGET_MODULE_ID).executeSync();
+            clientService.lol4("test4").onModule(TARGET_MODULE_ID).executeAsync(UUID.randomUUID().toString(), ServiceCallback.class);
+            personService.get(id).onModule(TARGET_MODULE_ID).executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
             personService.lol2("kek").executeSync();
             try {
-                personService.testError().onModule("main.server").executeSync();
+                personService.testError().onModule(TARGET_MODULE_ID).executeSync();
             } catch (Exception e) {
                 log.error("Exception during sync call:", e);
             }
-            personService.testError().onModule("main.server").executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
+            personService.testError().onModule(TARGET_MODULE_ID).executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
         };
 
-        for (int i = 0; i < CONCURRENCY_LEVEL; i++) {
-            threadList.add(new Thread(runnable));
-        }
-
-        threadList.forEach(Thread::start);
-
-        try {
-            threadList.forEach(thread -> {
-                try {
-                    thread.join();
-                } catch (Exception e) {
-                    log.error("Error occurred while stopping threads", e);
-                }
-            });
-            Thread.sleep(1000);
-        } catch (Exception ignore) {
-        }
-
+        for(int i = 0; i < CONCURRENCY_LEVEL; i++) executor.execute(runnable);
+        executor.awaitTermination(1, TimeUnit.MINUTES);
+        executor.shutdown();
         ctx.close();
-
         System.exit(0);
     }
 }
