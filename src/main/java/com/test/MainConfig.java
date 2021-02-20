51605073286600bae219f5086e8539695fbcf43e
edwardhyde <komesergey@gmail.com>
2021-02-20 22:01:41 +0300

    Updated to v1.11

--- src/main/java/com/test/MainConfig.java
+++ src/main/java/com/test/MainConfig.java
@@ -3,7 +3,11 @@ package com.test;
 import com.jaffa.rpc.lib.configuration.JaffaRpcConfig;
 import com.jaffa.rpc.lib.spring.ClientEndpoint;
 import com.jaffa.rpc.lib.spring.ServerEndpoints;
-import com.test.services.*;
+import com.test.services.ClientServiceClient;
+import com.test.services.ClientServiceImpl;
+import com.test.services.PersonServiceClient;
+import com.test.services.PersonServiceImpl;
+import com.test.services.TicketProviderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -27,5 +31,10 @@ public class MainConfig {
     @Bean
     public ClientEndpoint personEndpoint() {
         return new ClientEndpoint(PersonServiceClient.class);
+    }
+
+    @Bean
+    public String moduleId() {
+        return "main.server";
     }
 }
